8c2a0d6bc7ebcbed067addecf26de19b7e3407c7
Tim Killian <tskillian@gmail.com>
2014-08-18 09:57:12 -0700

    reworked the way collisions were tracked in order to fix some tricky bugs

--- public/main.js
+++ public/main.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
+	// TODO:
+	// there's a bug where if you add a new collision, old collisions 'go away'
+	// 
+
 	var clickedBox = null;
-	// numberCollisionBoxes is to hold any boxes where the numbers are not possible (i.e. two 9s in a row)
-	var numberCollisionBoxes = [];
+	// collisionMap is to hold any boxes where the numbers are not possible (i.e. two 9s in a row)
+	var collisionMap = {};
+	var idToNumCollisionsMap = {};
 
 	function resetClickedBox() {
 		if (clickedBox) {
@@ -25,35 +30,105 @@ $(document).ready(function() {
 		numberCollisionBoxes = [];
 	}
 
-	function updateNumberCollisionBoxes(inputElement, number) {
-		returnNumberCollisionBoxesToOriginalState();
+	function updateInfo() {
+		// console.log('numcollisions:', numberCollisionBoxes.length)
+		// console.log('numfilledinboxes', numFilledInBoxes);
+		// // TODO count filled in boxes continuously rather than all at each updateInfo invocation
+		// var numFilledInBoxes = 0;
+		// $('input').each(function (index, element) {
+		// 	if ($(element).val()) { numFilledInBoxes += 1; }
+		// });
+		// console.log(numFilledInBoxes)
+		// if (numberCollisionBoxes.length > 0) {
+		// 	$('.info').text('There are currently number collisions');
+		// } else if (numberCollisionBoxes.length === 0 && numFilledInBoxes === 81) {
+		// 	$('.info').text('Congratulations! You have successfully completed the puzzle!');
+		// } else {
+		// 	$('.info').text('');
+		// }
+	}
+
+	function getCollisions(inputElement, number) {
 		var parent = inputElement.parent();
-		var columnClassName = parent.attr('class');
+		var parentClasses = parent.attr('class').split(' '); // first class is column, second is subgrid
 		var rowClassName = parent.parent().attr('class');
-		var columns = $('.' + columnClassName + ' input');
+		var columns = $('.' + parentClasses[0] + ' input');
 		var rows = $('.' + rowClassName + ' input');
-		var isCurrentlyACollision = false;
+		var subgrid = $('.' + parentClasses[1] + ' input');
+		var inputElementUniqueId = parent.attr('id');
+		var collisions = [];
+		var collisionIdSet = {}; // to ensure there are never duplicate collision elements
 		for (var i = 0; i < 9; i++) {
-			// both columns and rows will always have 9 elements
 			var tempColumnBox = $(columns[i]);
 			var tempRowBox = $(rows[i]);
-			if (tempColumnBox.val() == number) {
-				tempColumnBox.addClass('collision');
-				numberCollisionBoxes.push(tempColumnBox);
-				isCurrentlyACollision = true;
+			var tempSubgridBox = $(subgrid[i]);
+			if (tempColumnBox.val() == number && !tempColumnBox.is(inputElement)) {
+				var tempId = tempColumnBox.parent().attr('id');
+				
+				if (!(tempId in collisionIdSet)) {
+					tempColumnBox.addClass('collision');
+					collisions.push(tempColumnBox);
+					collisionIdSet[tempId] = true; // true is arbitrary
+				}
 			}
 
-			if (tempRowBox.val() == number) {
-				tempRowBox.addClass('collision');
-				numberCollisionBoxes.push(tempRowBox);
-				isCurrentlyACollision = true;
+			if (tempRowBox.val() == number && !tempRowBox.is(inputElement)) {
+				var tempId = tempRowBox.parent().attr('id');
+				if (!(tempId in collisionIdSet)) {
+					tempRowBox.addClass('collision');
+					collisions.push(tempRowBox);
+					collisionIdSet[tempId] = true; // true is arbitrary
+				}
+			}
+
+			if (tempSubgridBox.val() == number && !tempSubgridBox.is(inputElement)) {
+				var tempId = tempSubgridBox.parent().attr('id');
+				if (!(tempId in collisionIdSet)) {
+					console.log('add subgrid')
+					tempSubgridBox.addClass('collision');
+					collisions.push(tempSubgridBox);
+					collisionIdSet[tempId] = true; // true is arbitrary
+				}
 			}
 		}
-		// TODO check local grid for duplicate num
 
-		if (isCurrentlyACollision) {
+		return collisions;
+	}
+
+	function updateNumberCollisionBoxes(inputElement, number) {
+		var id = inputElement.parent().attr('id');
+		// if there are collisions for this element, clear them
+		if (id in collisionMap) {
+			var collisionIdsToClear = Object.keys(collisionMap[id]);
+			for (var i = 0; i < collisionIdsToClear.length; i++) {
+				var collisionElementId = collisionIdsToClear[i];
+				collisionElement = collisionMap[id][collisionElementId];
+				idToNumCollisionsMap[collisionElementId] -= 1;
+				// only remove collision class if collision element if its number of collisions is 0
+				if (idToNumCollisionsMap[collisionElementId] === 0) {
+					collisionElement.removeClass('collision');
+					console.log('reduce id collisions by 1:', idToNumCollisionsMap[id])
+					console.log(idToNumCollisionsMap)
+				}
+				
+			}
+			delete collisionMap[id];
+			$('#' + id + ' input').removeClass('collision');
+		}
+		
+		var collisions = getCollisions(inputElement, number);
+		idToNumCollisionsMap[id] = collisions.length;
+
+		if (collisions.length > 0) {
+			collisionMap[id] = {}; // this obj will be a set of collisions
 			inputElement.addClass('collision');
-			numberCollisionBoxes.push(inputElement);
+		}
+
+		for (var i = 0; i < collisions.length; i++) {
+			var tempId = collisions[i].parent().attr('id');
+			collisionMap[id][tempId] = collisions[i];
+			if (!(tempId in idToNumCollisionsMap)) { idToNumCollisionsMap[tempId] = 0; }
+			idToNumCollisionsMap[tempId] += 1;
 		}
 	}
 
@@ -79,6 +154,10 @@ $(document).ready(function() {
 			// prevent defalt on everything but backspace/delete so we can dictate what goes into the input field
 			if (event.keyCode !== 8 && event.keyCode !== 46) {
 				event.preventDefault();
+			} else {
+				// still need to update collision boxes if the user removes a number with backspace/delete
+				updateNumberCollisionBoxes(clickedBox, null);
+				updateInfo();
 			}
 
 			// pressedKey will either be a number or NaN here
@@ -86,6 +165,7 @@ $(document).ready(function() {
 			if (pressedKey > 0) {
 				updateNumberCollisionBoxes(clickedBox, pressedKey);
 				clickedBox.val(String.fromCharCode(event.keyCode));
+				updateInfo();
 			}
 		} else {
 			event.preventDefault();
